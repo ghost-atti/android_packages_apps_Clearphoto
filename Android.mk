8566011102da731a317aa3f8e056847538ed697f
AttiRC <atti.mom.s2@gmail.com>
2013-10-11 17:45:02 +0900

    name change, api version fix, something patched

--- Android.mk
+++ Android.mk
@@ -17,6 +17,6 @@ LOCAL_MODULE_TAGS := optional
 LOCAL_STATIC_JAVA_LIBRARIES := \
     android-support-v4
 
-LOCAL_PACKAGE_NAME := CleartypeWallpapers
+LOCAL_PACKAGE_NAME := Clearphoto
 
 include $(BUILD_PACKAGE)
